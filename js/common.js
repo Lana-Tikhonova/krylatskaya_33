b786db6a51e0735e93d2a77a6839c2d81b8847d5
Svetlana <frontend.lana@gmail.com>
2025-02-06 10:42:08 +0500

    fix

--- js/common.js
+++ js/common.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
         watchSlidesVisibility: true,
         effect: 'slide',
         speed: 1000,
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
